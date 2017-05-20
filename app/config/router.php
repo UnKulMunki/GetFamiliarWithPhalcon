ed3111cd09ee752a64115126c1fdbbf5a1141cb9
Gary Cartagena <GaryC@CompGu.com>
2017-05-19 23:53:43 -0500

    removed the default application index page in favor of a default route

--- app/config/router.php
+++ app/config/router.php
@@ -1,7 +1,18 @@
 <?php
 
+/**
+ * Load shared configuration service
+ */
+$di->setShared(
+    'config', function () {
+        return include APP_PATH . "/config/config.php";
+    }
+);
+
 $router = $di->getRouter();
+$config = $di->getConfig();
 
 // Define your routes here
+$router->setDefaultController($config->application->defaultController);
 
 $router->handle();
